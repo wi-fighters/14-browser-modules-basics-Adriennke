463e9174a771722fadc609eed47de685b85151db
Adrienn <adrienntollas@gmail.com>
2020-02-12 13:39:22 +0100

    import js files to index.js and add it to html

---
+++ modules/index.js
@@ -0,0 +1,13 @@
+import {
+    scrollWindow
+} from ".modules/scroll.js";
+import {
+    clickImages
+} from ".modules/click.js";
+import {
+    hoverHeader
+} from ".modules/hover.js";
+
+scrollWindow();
+clickImages();
+hoverHeader();
